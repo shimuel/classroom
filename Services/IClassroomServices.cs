4754e9fb96d8fafb5f3492ce2ce942d0103e9597
Gaurav Yeole <gauravyeole@gmail.com>
2020-06-21 17:19:41 -0700

    Implement Classroom Service

--- Services/IClassroomServices.cs
+++ Services/IClassroomServices.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace ClassroomApp.Services
 {
-    interface IClassroomService
+    public interface IClassroomService
     {
         bool AddStudent(Student student);
 
